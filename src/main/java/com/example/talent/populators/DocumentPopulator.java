a3ca75afc9c7b17b2e932b394d42d3a5c1ee9cba
Thomas Atchapero <t.atchapero@gmail.com>
2026-02-27 07:50:16 +0100

    Refactored DocumentPopulator.java

--- src/main/java/com/example/talent/populators/DocumentPopulator.java
+++ src/main/java/com/example/talent/populators/DocumentPopulator.java
@@ -4,29 +4,35 @@ import com.example.talent.dtos.DocumentDTO;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Base64;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class DocumentPopulator {
     public static List<DocumentDTO> populateDocuments() {
-        String name = "Resume.pdf";
+        List<String> documentNames = Arrays.asList(
+                "Thomas_Atchapero_Ansøgning_Tech_Chapter.pdf",
+                "Thomas_Atchapero_CV.pdf"
+        );
+
+        return documentNames.stream().map((d) -> DocumentDTO.builder()
+                .id(UUID.randomUUID())
+                .name(d)
+                .content(encodeDocument(d))
+                .build())
+                .collect(Collectors.toList());
+    }
+
+    private static String encodeDocument(String name) {
         ClassPathResource resource = new ClassPathResource("documents/" + name);
-        String base64;
 
         try {
             byte[] bytes = resource.getInputStream().readAllBytes();
-            base64 = Base64.getEncoder().encodeToString(bytes);
+            return Base64.getEncoder().encodeToString(bytes);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-        DocumentDTO document = DocumentDTO.builder()
-                .id(UUID.randomUUID())
-                .name(name)
-                .content(base64)
-                .build();
-
-        return List.of(document);
     }
 }
